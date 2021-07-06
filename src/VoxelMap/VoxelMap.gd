7192517fb2cf348034b25c4bdc3d7cd5c73412c0
SPOICAT <spoicat15@gmail.com>
2021-07-06 07:53:52 +0600

    Updated VoxelMap.gd with `remove_last_cell()` function

--- src/VoxelMap/VoxelMap.gd
+++ src/VoxelMap/VoxelMap.gd
@@ -1,8 +1,13 @@
 extends GridMap
 
 
+export var generate_on_ready : bool = true
+
+export var player_path : NodePath
+onready var player = get_node(player_path)
+
 var noise = OpenSimplexNoise.new()
-var size : Vector3 = Vector3(50, 25, 50)
+var size : Vector3 = Vector3(64, 32, 64)
 
 
 enum {
@@ -10,9 +15,14 @@ enum {
 }
 
 
+func _physics_process(_delta):
+	pass
+
+
 func _ready():
 	noise.seed = randi()
-	generate()
+	if generate_on_ready:
+		generate()
 	
 	
 func generate():
@@ -22,3 +32,10 @@ func generate():
 				
 				if y < noise.get_noise_2d(x, z) * 5+10:
 					set_cell_item(x, y, z, GRASS)
+
+
+func remove_last_cell():
+	var last = get_used_cells().max()
+	if last:
+		for y in size.y:
+			set_cell_item(last.x, y, last.z, -1)
