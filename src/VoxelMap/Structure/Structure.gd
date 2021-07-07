a9f15a5720a910fe5ee24b31a5c58aaae77d4f5c
SPOICAT <spoicat15@gmail.com>
2021-07-07 11:57:03 +0600

    Updated `Structure` with re-usable building function

--- src/VoxelMap/Structure/Structure.gd
+++ src/VoxelMap/Structure/Structure.gd
@@ -7,3 +7,32 @@ class_name Structure
 
 onready var container = get_parent()
 onready var voxelmap = container.get_node(container.voxelmap_path)
+
+
+# example dictionary for structure dicts that contain data for structure blocks
+var _eg_dict = {
+    "height": 0,
+    "widthx": 0,
+    "widthz": 0,
+    "top": 0,
+    "left": 0,
+    "forward": 0,
+    "block": 0
+}
+
+
+# builds the dict
+func build(dict):
+    var pos = voxelmap.world_to_map(global_transform.origin)
+    
+    for x in dict["widthx"]:
+        for y in dict["height"]:
+            for z in dict["widthz"]:
+
+                var calculated = (
+                    pos.x + x + dict["left"],
+                    pos.y + y + dict["top"],
+                    pos.z + z + dict["forward"]
+                )
+
+                voxelmap.set_cell_item(calculated.x, calculated.y, calculated.z, dict["block"])
